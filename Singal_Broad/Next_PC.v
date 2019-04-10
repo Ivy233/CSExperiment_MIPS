3397fc0cba2b2d884ba0a363c4be8248ea396ec0
Ivy233 <952254420@qq.com>
2019-04-10 16:34:02 +0800

    v0.3 tmp

---
+++ Singal_Broad/Next_PC.v
@@ -0,0 +1,27 @@
+module Next_PC(pc_cur, op, funct, branch_delta, jumpToWhere, aluout, pc_next);
+    input[31:0] aluout,pc_cur;
+    input[15:0] branch_delta;
+    input[25:0] jumpToWhere;
+    input[5:0] op, funct;
+    output reg[31:0] pc_next;
+
+    parameter 	BEQ				= 6'b000100,
+				BNE				= 6'b000101,
+				J				= 6'b000010;
+    wire[31:0] pc4;
+    assign pc4 = pc_cur + 3'b100;
+    always @(*) begin
+        case (op)
+            BEQ:begin
+                pc_next = pc4 + (aluout==32'b00000000 ? { {14{branch_delta[15]}}, branch_delta[15:0],2'b00} : 0);
+            end
+            BNE:begin
+                pc_next = pc4 + (aluout!=32'b00000000 ? { {14{branch_delta[15]}}, branch_delta[15:0],2'b00} : 0);
+            end
+            J:begin
+                pc_next = {pc_cur[31:28], jumpToWhere[25:0], 2'b00};
+            end
+            default  pc_next = pc4;
+        endcase
+    end
+endmodule // Next_PC
---
+++ Singal_Broad/TESTBENCH.v
@@ -0,0 +1,55 @@
+`include "seg7x16.v"
+`include "clk_div.v"
+`include "MIPS.v"
+module TESTBENCH(
+    input clk,
+    input rst,
+    input[15:0] sw_i;
+    output[15:0] led_o;
+    output[7:0] o_seg,
+    output[7:0] o_sel
+);
+    wire clk_display = clk;
+    wire clk_CPU;
+    wire rst_CPU = ~rst;
+    reg[31:0] clk_cnt;
+
+    initial begin
+        $dumpfile("MIPS.vcd");
+        $dumpvars;
+        $readmemh("Bubble_sort.txt", U_MIPS.U_Instr_Mem.Instrs);
+        clk_cnt = 0;
+    end
+
+    always @(posedge rst_CPU) begin
+        clk_cnt=0;
+    end
+    always @(posedge clk_CPU) begin
+        clk_cnt = clk_cnt + 1;
+        if(clk_cnt >= 2048)begin
+            $finish;
+        end
+    end
+
+    MIPS U_MIPS(
+        .clk(clk_CPU),
+        .rst(rst_CPU),
+        .Ctrl_out(led_o)
+    );
+
+    clk_div U_clk_div(
+        .clk(clk_display),
+        .rst(rst_CPU),
+        .sw_15(sw_i),
+        .clk_CPU(clk_CPU)
+    );
+
+    wire[31:0] display_data=(U_MIPS.Ctrl_regWr ? U_MIPS.Mem_RegFile_write_data : (U_MIPS.Ctrl_MemWr ? U_MIPS.reg_out2 : U_MIPS.pc_cur))
+    seg7x16 U_seg7x16(
+        .clk(clk_display),
+        .rst(rst_CPU),
+        .i_data(display_data),
+        .o_seg(o_seg),
+        .o_sel(o_sel)
+    );
+endmodule // TESTBENCH

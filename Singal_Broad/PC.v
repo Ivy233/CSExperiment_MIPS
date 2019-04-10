bd2740044bb62221e05c9841205aa72da1e2e938
Ivy233 <952254420@qq.com>
2019-04-10 15:16:11 +0800

    v0.3 broad_start

--- Singal_Broad/PC.v
+++
@@ -1,9 +0,0 @@
-module PC(clk, rst, pc_next, pc_cur);
-    input clk, rst;
-    input[31:0] pc_next;
-    output reg[31:0] pc_cur;
-
-    always@ (posedge clk or posedge rst)begin
-        pc_cur = rst ? 32'h00003000 : pc_next;
-    end
-endmodule //pc
